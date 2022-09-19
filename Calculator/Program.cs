4cd4facbc6a63889e5d2b1f5618c4df3de20ae88
Aliaksandr_Lukashevi <aliaksandr_lukashevich@epam.com>
2022-09-19 18:21:22 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            double num1 = 0; double num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            string op = Console.ReadLine();
+
+            double result = Calculation.DoOperation(num1, num2, op);
+
+
+            Console.WriteLine("Your result: {0:0.##}\n", result);
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+    }
+}
